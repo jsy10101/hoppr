f8cc8aa83a3449f97ae7cba9be71f74f3666c960
jsy10101 <jaskiratchawla580@gmail.com>
2025-09-12 19:59:51 -0400

    HOPPR | create http server for trip-service

--- services/api-gateway/http.go
+++ services/api-gateway/http.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"log"
 	"net/http"
 	"ride-sharing/shared/contracts"
 )
 
 func handleTripPreview(w http.ResponseWriter, r *http.Request) {
 	var reqBody previewTripRequest
-
 	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
 		http.Error(w, "failed to parse JSON data", http.StatusBadRequest)
 		return
@@ -22,7 +23,25 @@ func handleTripPreview(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	jsonBody, _ := json.Marshal(reqBody)
+	reader := bytes.NewReader(jsonBody)
+
 	// TODO: Call trip service
-	response := contracts.APIResponse{Data: "ok bbys"}
+	resp, err := http.Post("http://trip-service:8083/preview", "application/json", reader)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	var respBody any
+	if err := json.NewDecoder(resp.Body).Decode(&respBody); err != nil {
+		http.Error(w, "failed to parse JSON data from trip service", http.StatusBadRequest)
+		return
+	}
+
+	response := contracts.APIResponse{Data: respBody}
+
 	writeJSON(w, http.StatusCreated, response)
 }
